408d6fcf297b2ec8da615390187200d8e91b8116
Irwing Joshua <joshuairwing@gmail.com>
2021-02-11 21:46:01 -0300

    First commit.

---
+++ R/aritmetica.R
@@ -0,0 +1,29 @@
+#' Function to sum
+#'
+#' This function is going to sum.
+#'
+#' @param a A number.
+#' @param b It's another number.
+#'
+#' @examples
+#' sum (2, 3)
+#'
+#'@export
+somar <- function(a, b) {
+  sum (a,b)
+}
+
+#' Function to subtract
+#'
+#' This function is going to subtract.
+#'
+#' @param a A number.
+#' @param b It's another number.
+#'
+#' @examples
+#' subtrair (2, 3)
+#'
+#'@export
+subtrair <- function(a, b){
+  a-b
+}
